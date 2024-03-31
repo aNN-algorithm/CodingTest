f993114ffe5c89a99c6aa8e8d87dcf0808d4e71e
aNN-algorithm <149382038+aNN-algorithm@users.noreply.github.com>
2024-03-31 13:40:26 +0900

    [Silver III] Title: N과 M (1), Time: 2376 ms, Memory: 75756 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,39 +1,45 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int n = 0;
-    static int m = 0;
-    static int[] arr = new int[10];
-    static boolean[] isUsed = new boolean[10];
+    static int N;
+    static int M;
+    static boolean[] visited;
+    static int[] arr;
 
-    static void func(int cur) {
-        if (cur == m) {
-            for (int i = 0; i < m; i++) {
+    public static void func(int cur) {
+        if (cur == M) {
+            for (int i = 0; i < M; i++) {
                 System.out.print(arr[i] + " ");
             }
             System.out.println();
             return;
         }
 
-        for (int i = 1; i <= n; i++) {
-            if (isUsed[i] == true) {
+
+        for (int i = 1; i <= N; i++) {
+            if (visited[i] == true) {
                 continue;
             }
+            visited[i] = true;
             arr[cur] = i;
-            isUsed[i] = true;
             func(cur + 1);
-            isUsed[i] = false;
+            visited[i] = false;
         }
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String comm = br.readLine();
+        StringTokenizer st = new StringTokenizer(comm);
 
-        n = sc.nextInt();
-        m = sc.nextInt();
-        sc.nextLine();
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        visited = new boolean[N + 1];
+        arr = new int[N];
 
         func(0);
-
     }
 }
