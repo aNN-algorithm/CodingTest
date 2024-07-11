829a2940cb7fb7c424e76613062f06728d31b0dd
Ahn Yeonjeong <149382038+aNN-algorithm@users.noreply.github.com>
2024-07-11 13:33:42 +0900

    [Silver I] Title: 절댓값 힙, Time: 484 ms, Memory: 27056 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<int[]> queue = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0]) {
+                    return Integer.compare(o1[1], o2[1]);
+                }
+                return Integer.compare(o1[0], o2[0]);
+            }
+        });
+
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                if (queue.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    int[] arr = queue.poll();
+                    System.out.println(arr[1]);
+                }
+                continue;
+            }
+            if (num < 0) {
+                queue.add(new int[] {num * -1, num});
+            } else if (num > 0) {
+                queue.add(new int[] {num, num});
+            }
+        }
+
+    }
+}
