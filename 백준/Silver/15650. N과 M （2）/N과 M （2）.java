a629c3991c81fbc19b52a3ba817b14091675c16a
Ahn Yeonjeong <149382038+aNN-algorithm@users.noreply.github.com>
2024-08-08 17:09:03 +0900

    [Silver III] Title: N과 M (2), Time: 100 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -4,34 +4,44 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
     static int M;
+    static int N;
     static int[] arr;
-
-    public static void func(int x, int cur) {
-        if (cur == M) {
-            for (int i = 0; i < M; i++) {
-                System.out.print(arr[i] + " ");
-            }
-            System.out.println();
-            return;
-        }
-
-        for (int i = x; i <= N; i++) {
-            arr[cur] = i;
-            func(i + 1, cur + 1);
-        }
-    }
+    static boolean[] isUsed;
+    static StringBuilder sb;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String comm = br.readLine();
         StringTokenizer st = new StringTokenizer(comm);
+        sb = new StringBuilder();
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+        isUsed = new boolean[N + 1];
         arr = new int[N];
 
         func(1, 0);
+        System.out.println(sb);
+    }
+
+    public static void func(int x, int cur) {
+        if (cur == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = x; i <= N; i++) {
+            if (isUsed[i]) {
+                continue;
+            }
+            arr[cur] = i;
+            isUsed[i] = true;
+            func(i, cur + 1);
+            isUsed[i] = false;
+        }
     }
 }
