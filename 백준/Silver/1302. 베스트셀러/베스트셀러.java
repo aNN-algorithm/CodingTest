15b668a964c3325774dffa5b100362cb5732e09f
Ahn Yeonjeong <149382038+aNN-algorithm@users.noreply.github.com>
2024-08-18 17:37:18 +0900

    [Silver IV] Title: 베스트셀러, Time: 100 ms, Memory: 14432 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Map<String, Integer> map = new HashMap<>();
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            String sold = br.readLine();
+            map.put(sold, map.getOrDefault(sold, 0) + 1);
+            max = Math.max(max, map.get(sold));
+        }
+        List<String> list = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == max) {
+                list.add(entry.getKey());
+            }
+        }
+        Collections.sort(list);
+        System.out.println(list.get(0));
+    }
+}
