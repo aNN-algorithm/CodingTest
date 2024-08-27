c4852ece2719700134e5d00f813a43596022060a
Ahn Yeonjeong <149382038+aNN-algorithm@users.noreply.github.com>
2024-08-27 15:38:33 +0900

    [Silver IV] Title: 동전 0, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,30 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String comm = br.readLine();
-        StringTokenizer st = new StringTokenizer(comm);
+        String[] comm = br.readLine().split(" ");
+        int n = Integer.parseInt(comm[0]);
+        int K = Integer.parseInt(comm[1]);
 
-        int T = Integer.parseInt(st.nextToken());
-        int[] coin = new int[T];
-        int sum = Integer.parseInt(st.nextToken());
-
-        for (int i = 0; i < T; i++) {
-            coin[i] = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
-        int cnt = 0;
-        for (int i = T - 1; i >= 0; i--) {
-            if (coin[i] <= sum) {
-                cnt = cnt + sum / coin[i];
-                sum %= coin[i];
+        int tmp = K;
+        int answer = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (arr[i] <= tmp) {
+                answer += tmp / arr[i];
+                tmp %= arr[i];
             }
         }
-
-        System.out.println(cnt);
+        System.out.println(answer);
     }
 }
