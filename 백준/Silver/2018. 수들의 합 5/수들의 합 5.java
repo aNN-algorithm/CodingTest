6faa6f10883b74da3b6943a71687e408c067c6f3
Ahn Yeonjeong <149382038+aNN-algorithm@users.noreply.github.com>
2024-09-04 17:03:58 +0900

    [Silver V] Title: 수들의 합 5, Time: 196 ms, Memory: 17680 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -3,23 +3,27 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
         int answer = 0;
-        for (int i = 1; i < n / 2 + 1; i++) {
-            int sum = 0;
-            for (int j = i; j <= n / 2 + 1; j++) {
-                sum += j;
-                if (sum > n) {
-                    break;
-                }
-                if (sum == n) {
-                    answer++;
-                }
+        
+        int start = 1;
+        int end = 1;
+        int sum = 1;
+        
+        while (start <= end) {
+            if (sum == n) answer++;
+            if (sum < n) {
+                end++;
+                sum += end;
+            }
+            else if (sum >= n) {
+                sum -= start;
+                start++;
             }
         }
-        System.out.println(answer + 1);
+
+        System.out.println(answer);
     }
 }
