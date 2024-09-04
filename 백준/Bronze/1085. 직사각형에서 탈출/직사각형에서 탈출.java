1dd4b516bb28f7e05756c6fa868446258a4aa96a
Ahn Yeonjeong <149382038+aNN-algorithm@users.noreply.github.com>
2024-09-04 11:40:56 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 100 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+
+        int x = Integer.parseInt(input[0]);
+        int y = Integer.parseInt(input[1]);
+        int w = Integer.parseInt(input[2]);
+        int h = Integer.parseInt(input[3]);
+
+        System.out.println(Math.min(Math.min(Math.abs(x - w), x)
+                , Math.min(Math.abs(y - h), Math.abs(y))));
+    }
+}
