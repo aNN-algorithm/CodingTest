bfc75f81b90aedcc42cd36ef69f679511aa52047
Ahn Yeonjeong <149382038+aNN-algorithm@users.noreply.github.com>
2024-09-04 17:39:52 +0900

    [Silver II] Title: 참외밭, Time: 100 ms, Memory: 14064 KB -BaekjoonHub

--- 백준/Silver/2477. 참외밭/참외밭.java
+++ 백준/Silver/2477. 참외밭/참외밭.java
@@ -9,6 +9,8 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
+        // 입력되는 수(1, 2, 3, 4)를 인덱스로 사용하기 위해
+        // 크기가 7 / 5인 배열 선언
         int[] dir = new int[7];
         int[] len = new int[7];
         int[] dirCount = new int[5];
@@ -16,6 +18,7 @@ public class Main {
             String input = br.readLine();
             StringTokenizer st = new StringTokenizer(input);
 
+            // 입력되는 방향과 길이를 그대로 각각의 배열에 저장
             dir[i] = Integer.parseInt(st.nextToken());
             len[i] = Integer.parseInt(st.nextToken());
             dirCount[dir[i]]++;
@@ -27,10 +30,13 @@ public class Main {
             if (dirCount[dir[i]] != 1) {
                 continue;
             }
+            // 1번만 입력된 변이 큰 면접의 변이다.
             maxArea *= len[i];
             int next = i + 3 > 6 ? i - 3 : i + 3;
             minArea *= len[next];
         }
+        // 입력은 반시계방향으로 진행되므로, 
+        // 긴 변이 입력된 후 3번째로 입력되는 변이 짧은 면적의 변이다.
         System.out.println((maxArea - minArea) * n);
     }
 }
