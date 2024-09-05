d3d895794c2f00a44d6308d41b06222ba01b314f
Ahn Yeonjeong <149382038+aNN-algorithm@users.noreply.github.com>
2024-09-05 11:53:36 +0900

    [Silver I] Title: Z, Time: 100 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.java
+++ 백준/Silver/1074. Z/Z.java
@@ -3,36 +3,37 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static int answer = 0;
+
+    private static int answer = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] comm = br.readLine().split(" ");
+        
         int N = Integer.parseInt(comm[0]);
         int r = Integer.parseInt(comm[1]);
         int c = Integer.parseInt(comm[2]);
 
         int size = (int) Math.pow(2, N);
-        func(size, r, c);
+        func(size / 2, r, c);
         System.out.println(answer);
     }
 
-    public static void func(int size, int r, int c) {
-        if (size == 1) {
+    private static void func(int N, int r, int c) {
+        if (N == 0) {
             return;
         }
-
-        if (r < size/2 && c < size/2) {
-            func(size/2, r, c);
-        } else if (r < size/2 && c >= size/2) {
-            answer += size * size / 4;
-            func(size/2, r, c - size/2);
-        } else if (r >= size/2 && c < size/2) {
-            answer += size * size / 4 * 2;
-            func(size/2, r - size/2, c);
-        } else {
-            answer += size * size / 4 * 3;
-            func(size/2, r - size/2, c - size/2);
+        if (r < N && c < N ) {
+            func(N / 2, r, c);
+        } else if (r < N && c >= N ) {
+            answer += N * N;
+            func(N / 2, r, c - N);
+        } else if (r >= N && c < N ) {
+            answer += 2 * N * N;
+            func(N / 2, r - N , c);
+        } else if (r >= N && c >= N) {
+            answer += 3 * N * N;
+            func(N / 2, r - N, c - N);
         }
     }
 }
