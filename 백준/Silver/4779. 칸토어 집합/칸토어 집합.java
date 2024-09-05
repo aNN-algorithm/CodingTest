d99bb4462fd6570a90ede952b6ff6bfd6f85e34a
Ahn Yeonjeong <149382038+aNN-algorithm@users.noreply.github.com>
2024-09-05 16:02:13 +0900

    [Silver III] Title: 칸토어 집합, Time: 264 ms, Memory: 25376 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -1,13 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
+import java.util.Arrays;
 
 /**
  * 7번 - 칸토어 집합
  * https://www.acmicpc.net/problem/4779
  -----------------------------------------------------------------------------
- *
+ * 3의 세제곱으로 이루어진 배열을 3등분 하여 지울 위치를 찾아 지우고,
+ * 양 옆의 배열을 다시 재귀의 파라미터 값으로 넣어 반복
  -----------------------------------------------------------------------------
  */
 
@@ -28,11 +29,11 @@ public class Main {
                 size *= 3;
             }
             arr = new char[size];
-            for (int j = 0; j < arr.length; j++) {
-                arr[j] = '-';
-            }
+            Arrays.fill(arr, '-');
 
             func(0, size - 1);
+            
+            // 출력
             for (int j = 0; j < arr.length; j++) {
                 sb.append(arr[j]);
             }
@@ -42,9 +43,12 @@ public class Main {
     }
 
     private static void func(int x, int y) {
+        // base condition
         if (y == x) {
             return;
         }
+        
+        // 지울 위치 확인
         int start = x + (y - x + 1) / 3;
         int end = start + (y - x + 1) / 3;
         for (int i = start; i < end; i++) {
