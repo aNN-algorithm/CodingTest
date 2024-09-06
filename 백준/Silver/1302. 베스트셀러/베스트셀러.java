20421dbb37385433d03c80f79cdbf798b3a43bee
Ahn Yeonjeong <149382038+aNN-algorithm@users.noreply.github.com>
2024-09-06 13:57:15 +0900

    [Silver IV] Title: 베스트셀러, Time: 100 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Silver/1302. 베스트셀러/베스트셀러.java
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.java
@@ -1,7 +1,16 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
+
+/**
+ *
+ -----------------------------------------------------------------------------
+ * 8 : 베스트셀러(1302)
+ * https://www.acmicpc.net/problem/1302
+ * 1. Map 에 책을 사전순으로 제목과 판매 개수 저장
+ *  - 가장 많이 팔린 책의 개수를 저장 -> max
+ * 2. Map 을 순서대로 확인하여 max 와 같은 량으로 팔린 책 제목 출력
+ -----------------------------------------------------------------------------
+ */
 
 public class Main {
 
@@ -9,20 +18,26 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        Map<String, Integer> map = new HashMap<>();
+        // 사전순으로 정렬하여 Map에 저장 - TreeMap 사용
+        Map<String, Integer> map = new TreeMap<>();
         int max = 0;
         for (int i = 0; i < n; i++) {
-            String sold = br.readLine();
-            map.put(sold, map.getOrDefault(sold, 0) + 1);
-            max = Math.max(max, map.get(sold));
-        }
-        List<String> list = new ArrayList<>();
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            if (entry.getValue() == max) {
-                list.add(entry.getKey());
+            String book = br.readLine();
+            map.put(book, map.getOrDefault(book,0) + 1);
+
+            // 제일 많이 팔린 책의 개수 저장
+            if (max < map.get(book)) {
+                max = map.get(book);
             }
         }
-        Collections.sort(list);
-        System.out.println(list.get(0));
+
+        // 사전순으로 책 판매량을 보며, max값과 비교
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            // 가장 많이 팔린 책의 개수와 같은 개수로 팔린 책이 있다면 출력 후 반복문 종료
+            if (entry.getValue() == max) {
+                System.out.println(entry.getKey());
+                break;
+            }
+        }
     }
 }
