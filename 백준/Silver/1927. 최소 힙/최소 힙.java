c96064b0f9c1dcca94f13b6aef0f960096882f28
Ahn Yeonjeong <149382038+aNN-algorithm@users.noreply.github.com>
2024-09-26 13:12:38 +0900

    [Silver II] Title: 최소 힙, Time: 264 ms, Memory: 26656 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -1,27 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+// 6. 최소 힙
+// https://www.acmicpc.net/problem/1927
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
 
+        StringBuilder answer = new StringBuilder();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         for (int i = 0; i < n; i++) {
-            int num = Integer.parseInt(br.readLine());
-
-            if (num == 0) {
-                if (queue.isEmpty()) {
-                    System.out.println(0);
-                } else {
-                    System.out.println(queue.poll());
-                }
+            int input = Integer.parseInt(br.readLine());
+            if (input == 0) {
+                if (pq.isEmpty()) answer.append(0).append("\n");
+                else answer.append(pq.poll()).append("\n");
             } else {
-                queue.add(num);
+                pq.add(input);
             }
         }
+        System.out.println(answer);
     }
 }
