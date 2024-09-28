3f3e8ae022f8518c0cf3f7afb6933fe33a140241
Ahn Yeonjeong <149382038+aNN-algorithm@users.noreply.github.com>
2024-09-29 00:48:35 +0900

    [Silver IV] Title: Bucket Brigade, Time: 96 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Silver/17198. Bucket Brigade/Bucket Brigade.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static class Node {
+        int x;
+        int y;
+        int channel;
+
+        public Node(int x, int y, int channel) {
+            this.x = x;
+            this.y = y;
+            this.channel = channel;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[][] farm = new char[10][10];
+        boolean[][] isVisited = new boolean[10][10];
+
+        for (int i = 0; i < 10; i++) {
+            farm[i] = br.readLine().toCharArray();
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (farm[i][j] == 'L') {
+                    queue.add(new Node(i, j, 0));
+                    isVisited[i][j] = true;
+
+                    while (!queue.isEmpty()) {
+                        Node cur = queue.poll();
+
+                        int[] dx = {0, -1, 0, 1};
+                        int[] dy = {-1, 0, 1, 0};
+                        for (int k = 0; k < 4; k++) {
+                            int nx = cur.x + dx[k];
+                            int ny = cur.y + dy[k];
+
+                            if (nx < 0 || ny < 0 || nx >= 10 || ny >= 10) continue;
+                            if (farm[nx][ny] == 'R' || isVisited[nx][ny]) continue;
+                            if (farm[nx][ny] == 'B') {
+                                System.out.println(cur.channel);
+                                return;
+                            }
+
+                            queue.add(new Node(nx, ny, cur.channel + 1));
+                            isVisited[nx][ny] = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
