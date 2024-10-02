39f3fdc34cf3e0a8cf9b1ad7ad72f04dcc07b1ad
Ahn Yeonjeong <149382038+aNN-algorithm@users.noreply.github.com>
2024-10-02 13:03:00 +0900

    [Silver V] Title: 너의 평점은, Time: 108 ms, Memory: 14624 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.java
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -6,47 +6,28 @@ import java.util.*;
 public class Main {
 
     final static int SUBJECT_CNT = 20;
+    final static int GRADE_CNT = 9;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] grade = {"A+", "A0", "B+", "B0", "C+", "C0", "D+", "D0", "F"};
+        Double[] socre = {4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0.0};
 
         Double totalCredit = 0.0; // 학점의 총합
         Double totalScore = 0.0; // (학점 X 과목평점)의 합
         for (int i = 0; i < SUBJECT_CNT; i++) {
             String[] input = br.readLine().split(" ");
             Double credit = Double.parseDouble(input[1]);
-            String grade = input[2];
-            if (!grade.equals("P")) totalCredit += credit;
+            String result = input[2];
 
-            switch(grade) {
-                case "A+" :
-                    totalScore += credit * 4.5;
-                    break;
-                case "A0" :
-                    totalScore += credit * 4.0;
-                    break;
-                case "B+" :
-                    totalScore += credit * 3.5;
-                    break;
-                case "B0" :
-                    totalScore += credit * 3.0;
-                    break;
-                case "C+" :
-                    totalScore += credit * 2.5;
-                    break;
-                case "C0" :
-                    totalScore += credit * 2.0;
-                    break;
-                case "D+" :
-                    totalScore += credit * 1.5;
-                    break;
-                case "D0" :
-                    totalScore += credit * 1.0;
-                    break;
-                default :
-                    totalScore += credit * 0.0;
-                    break;
+            for (int j = 0; j < GRADE_CNT; j++) {
+                if (grade[j].equals(result)) {
+                    totalScore += credit * socre[j];
+                    totalCredit += credit;
+                }
             }
+
         }
         System.out.println(String.format("%.6f", totalScore / totalCredit));
     }
