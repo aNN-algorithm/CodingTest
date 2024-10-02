9808fbe29407518264da81c723da421e83a27c28
Ahn Yeonjeong <149382038+aNN-algorithm@users.noreply.github.com>
2024-10-02 12:04:13 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 104 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,25 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] comm = br.readLine().split("-");
+        String[] input = br.readLine().split("-");
 
-        int answer = 0;
-        for (int i = 0; i < comm.length; i++) {
-            String[] separated = comm[i].split("\\+");
-            int num = 0;
-            for (int j = 0; j < separated.length; j++) {
-                num += Integer.parseInt(separated[j]);
+        for (int i = 0; i < input.length; i++) {
+            String[] operands = input[i].split("\\+");
+            int temp = 0;
+            for (int j = 0; j < operands.length; j++) {
+                temp += Integer.parseInt(operands[j]);
             }
-            if (i == 0) {
-                answer += num;
-            } else {
-                answer -= num;
-            }
+            input[i] = String.valueOf(temp);
+        }
+
+        int answer = Integer.parseInt(input[0]);
+        for (int i = 1; i < input.length; i++) {
+            answer -= Integer.parseInt(input[i]);
         }
         System.out.println(answer);
     }
