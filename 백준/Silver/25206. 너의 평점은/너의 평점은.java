1402a5fb94906b54a3e9e2599907fe668a555ada
Ahn Yeonjeong <149382038+aNN-algorithm@users.noreply.github.com>
2024-10-02 12:56:56 +0900

    [Silver V] Title: 너의 평점은, Time: 116 ms, Memory: 14708 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.java
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -2,46 +2,52 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException{
+    final static int SUBJECT_CNT = 20;
 
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        double result = 0;
-        double sum = 0;
-        for (int i = 0; i < 20; i++) {
-            String[] grade = br.readLine().split(" ");
-            double score = Double.parseDouble(grade[1]);
-            String gd = grade[2];
-            if (!gd.equals("P")) {
-                sum += score;
 
-                if (gd.equals("A+")) {
-                    score *= 4.5;
-                } else if (gd.equals("A0")) {
-                    score *= 4.0;
-                } else if (gd.equals("B+")) {
-                    score *= 3.5;
-                } else if (gd.equals("B0")) {
-                    score *= 3.0;
-                } else if (gd.equals("C+")) {
-                    score *= 2.5;
-                } else if (gd.equals("C0")) {
-                    score *= 2.0;
-                } else if (gd.equals("D+")) {
-                    score *= 1.5;
-                } else if (gd.equals("D0")) {
-                    score *= 1.0;
-                } else if (gd.equals("F")) {
-                    score *= 0;
-                }
-                result += score;
+        Double totalCredit = 0.0; // 학점의 총합
+        Double totalScore = 0.0; // (학점 X 과목평점)의 합
+        for (int i = 0; i < SUBJECT_CNT; i++) {
+            String[] input = br.readLine().split(" ");
+            Double credit = Double.parseDouble(input[1]);
+            String grade = input[2];
+            if (!grade.equals("P")) totalCredit += credit;
+
+            switch(grade) {
+                case "A+" :
+                    totalScore += credit * 4.5;
+                    break;
+                case "A0" :
+                    totalScore += credit * 4.0;
+                    break;
+                case "B+" :
+                    totalScore += credit * 3.5;
+                    break;
+                case "B0" :
+                    totalScore += credit * 3.0;
+                    break;
+                case "C+" :
+                    totalScore += credit * 2.5;
+                    break;
+                case "C0" :
+                    totalScore += credit * 2.0;
+                    break;
+                case "D+" :
+                    totalScore += credit * 1.5;
+                    break;
+                case "D0" :
+                    totalScore += credit * 1.0;
+                    break;
+                default :
+                    totalScore += credit * 0.0;
+                    break;
             }
         }
-
-        System.out.printf("%.6f", result / sum);
+        System.out.println(String.format("%.6f", totalScore / totalCredit));
     }
 }
