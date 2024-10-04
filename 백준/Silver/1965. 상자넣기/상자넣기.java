877be0e6db2258e56314013443960d09e17d9071
Ahn Yeonjeong <149382038+aNN-algorithm@users.noreply.github.com>
2024-10-04 12:18:34 +0900

    [Silver II] Title: 상자넣기, Time: 128 ms, Memory: 14508 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] box = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            box[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n];
+        dp[0] = 1;
+        for (int i = 1; i < n; i++) {
+            int max = 0;
+            for (int j = 0; j < i; j++) {
+                if (box[j] < box[i]) {
+                    max = Math.max(max, dp[j]);
+                }
+            }
+            dp[i] = max + 1;
+        }
+
+        Arrays.sort(dp);
+        System.out.println(dp[n - 1]);
+    }
+}
