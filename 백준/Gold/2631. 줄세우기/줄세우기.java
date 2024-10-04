9271fc9f90ac900e9f0b0958148ab0025b89c855
Ahn Yeonjeong <149382038+aNN-algorithm@users.noreply.github.com>
2024-10-04 13:23:54 +0900

    [Gold IV] Title: 줄세우기, Time: 100 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] dp = new int[n];
+        dp[0] = 1;
+        for (int i = 1; i < n; i++) {
+            int max = 0;
+
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    max = Math.max(max, dp[j]);
+                }
+            }
+            dp[i] = max + 1;
+        }
+
+        Arrays.sort(dp);
+        System.out.println(n - dp[n - 1]);
+    }
+}
