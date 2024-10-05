7ab2b3be5837f2135b3c6467e88b9d22be0aa342
Ahn Yeonjeong <149382038+aNN-algorithm@users.noreply.github.com>
2024-10-05 18:32:05 +0900

    [Silver IV] Title: 게임을 만든 동준이, Time: 100 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] scores = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            scores[i] = Integer.parseInt(br.readLine());
+        }
+
+        int answer = 0;
+        for (int i = n - 2; i >= 0; i--) {
+            if (scores[i] >= scores[i + 1]) {
+                answer += scores[i] - scores[i + 1] + 1;
+                scores[i] = scores[i + 1] - 1;
+            }
+        }
+        System.out.println(answer);
+    }
+}
