49004c990b9cd5384ddec74e491c8d993d16bb81
Ahn Yeonjeong <149382038+aNN-algorithm@users.noreply.github.com>
2024-10-08 09:08:31 +0900

    [Silver V] Title: 문서 검색, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.java
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -6,27 +6,14 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String doc = br.readLine();
-        String search = br.readLine();
+        String origin  = br.readLine();
+        String word = br.readLine();
+
+        String result = origin.replace(word, "*");
 
         int answer = 0;
-        for (int i = 0; i < doc.length(); i++) {
-            if (doc.charAt(i) == search.charAt(0)) {
-                boolean isFind = true;
-                if (i + search.length() > doc.length()) {
-                    break;
-                }
-                for (int j = 0; j < search.length(); j++) {
-                    if (doc.charAt(i + j) != search.charAt(j)) {
-                        isFind = false;
-                        break;
-                    }
-                }
-                if (isFind) {
-                    answer++;
-                    i += search.length() - 1;
-                }
-            }
+        for (int i = 0; i < result.length(); i++) {
+            if (result.charAt(i) == '*') answer++;
         }
         System.out.println(answer);
     }
