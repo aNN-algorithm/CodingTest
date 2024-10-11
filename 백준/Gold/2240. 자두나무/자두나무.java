30318c6942bc8995bc9ae883cdb0beec0636d62b
Ahn Yeonjeong <149382038+aNN-algorithm@users.noreply.github.com>
2024-10-12 00:30:27 +0900

    [Gold V] Title: 자두나무, Time: 104 ms, Memory: 14440 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.java
+++ 백준/Gold/2240. 자두나무/자두나무.java
@@ -9,16 +9,16 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int T = Integer.parseInt(st.nextToken());
         int W = Integer.parseInt(st.nextToken());
-        int[][][] dp = new int[T + 1][W + 1][3]; // 자두가 떨어지는 위치 | 이동 횟수 | 위치
+        int[][][] dp = new int[T + 1][3][W + 1]; // 자두가 떨어지는 위치 | 이동 횟수 | 위치
 
         int tree = Integer.parseInt(br.readLine());
 
         if (tree == 1) {
             // 처음에 1번 나무에서 떨어진다면?
-            dp[1][0][1] = 1; // 자두도 1번에서 시작
+            dp[1][1][0] = 1; // 자두도 1번에서 시작
         } else {
             // 처음 2번 나무에서 떨어진다면?
-            dp[1][1][2] = 1; // 자두는 2번으로 이동
+            dp[1][2][1] = 1; // 자두는 2번으로 이동
         }
 
         for (int i = 2; i < T + 1; i++) {
@@ -28,9 +28,9 @@ public class Main {
             if (tree == 1) { // 자두가 1번 나무에서 떨어진다면
                 // 여태 이동을 1번도 안 했을 때,
                 // 현재 나의 위치가 1번일 때,
-                dp[i][0][1] = dp[i - 1][0][1] + 1; // 자두를 얻음
+                dp[i][1][0] = dp[i - 1][1][0] + 1; // 자두를 얻음
                 // 현재 나의 위치가 2번일 때,
-                dp[i][0][2] = dp[i - 1][0][2]; // 자두를 얻지 못함
+                dp[i][2][0] = dp[i - 1][2][0]; // 자두를 얻지 못함
 
                 // W번 이동을 했을 때,
                 for (int j = 1; j < W + 1; j++) {
@@ -39,20 +39,20 @@ public class Main {
                     // - 1번이면 위치 유지 -> j 그대로
                     // - 2번이면 위치 이동 -> j - 1 이동 횟수 감소
                     // 그중 큰 것 + 1(자두를 얻음)
-                    dp[i][j][1] = Math.max(dp[i - 1][j][1], dp[i - 1][j - 1][2]) + 1;
+                    dp[i][1][j] = Math.max(dp[i - 1][1][j], dp[i - 1][2][j - 1]) + 1;
                     // 현재 나의 위치가 2번일 때,
                     // (i - 1)번째(이전)에
                     // - 1번이면 위치 이동 -> j - 1 이동 횟수 감소
                     // - 2번이면 위치 유지 -> j 그대로
                     // 그중 큰 것 (자두는 얻지 못함)
-                    dp[i][j][2] = Math.max(dp[i - 1][j - 1][1], dp[i - 1][j][2]);
+                    dp[i][2][j] = Math.max(dp[i - 1][1][j - 1], dp[i - 1][2][j]);
                 }
             } else { // 자두가 2번 나무에서 떨어진다면
                 // 여태 이동을 1번도 안 했을 때,
                 // 현재 나의 위치가 1번일 때,
-                dp[i][0][1] = dp[i - 1][0][1]; // 자두를 얻지 못함
+                dp[i][1][0] = dp[i - 1][1][0]; // 자두를 얻지 못함
                 // 현재 나의 위치가 2번일 때
-                dp[i][0][2] = dp[i - 1][0][2] + 1; // 자두를 얻음
+                dp[i][2][0] = dp[i - 1][2][0] + 1; // 자두를 얻음
 
                 // W번 이동을 했을 때
                 for (int j = 1; j < W + 1; j++) {
@@ -61,13 +61,13 @@ public class Main {
                     // - 1번이면 위치 유지 -> j 그대로
                     // - 2번이면 위치 이동 -> j - 1 이동 횟수 감소
                     // 그중 큰 것 (자두는 얻지 못함)
-                    dp[i][j][1] = Math.max(dp[i - 1][j][1], dp[i - 1][j - 1][2]);
+                    dp[i][1][j] = Math.max(dp[i - 1][1][j], dp[i - 1][2][j - 1]);
                     // 현재 나의 위치가 2번일 때
                     // (i - 1)번째(이전)에
                     // - 1번이면 위치 이동 -> j - 1 이동 횟수 감소
                     // - 2번이면 위치 유지 -> j 그대로
                     // 그중 큰 것 (자두를 얻음)
-                    dp[i][j][2] = Math.max(dp[i - 1][j - 1][1], dp[i - 1][j][2]) + 1;
+                    dp[i][2][j] = Math.max(dp[i - 1][1][j - 1], dp[i - 1][2][j]) + 1;
                 }
             }
         }
@@ -76,7 +76,7 @@ public class Main {
         // 모든 이동의 횟수 중 1번과 2번 중 큰 값이 얻을 수 있는 자두의 가장 큰 수
         int answer = 0;
         for (int i = 0; i < W + 1; i++) {
-            answer = Math.max(answer, Math.max(dp[T][i][1], dp[T][i][2]));
+            answer = Math.max(answer, Math.max(dp[T][1][i], dp[T][2][i]));
         }
         System.out.println(answer);
     }
