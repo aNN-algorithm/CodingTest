950b6cdda3beddcb9a1ad9761f44c4f1e16f41f9
Ahn Yeonjeong <149382038+aNN-algorithm@users.noreply.github.com>
2025-01-02 11:52:30 +0900

    [level 1] Title: 신고 결과 받기, Time: 172.95 ms, Memory: 186 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/README.md
+++ 프로그래머스/1/92334. 신고 결과 받기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 신고 결과 받기 - 92334 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92334#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92334) 
 
 ### 성능 요약
 
-메모리: 74.6 MB, 시간: 0.21 ms
+메모리: 186 MB, 시간: 172.95 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 03일 22:48:33
+2025년 01월 02일 11:52:26
 
 ### 문제 설명
 
--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,46 +1,36 @@
-
 import java.util.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
 
-        // 중복 신고를 제거한 신고 리스트
         List<String> noRedun = new ArrayList<>(new HashSet<>(Arrays.asList(report)));
-
-        // 각 사용자가 몇 번 신고를 받았는지 저장하는 맵
         Map<String, Integer> reportCnt = new HashMap<>();
-
-        // 각 사용자가 누구를 신고했는지 기록하는 맵
         Map<String, List<String>> reportLog = new HashMap<>();
 
-        // reportLog 맵 초기화
-        for (String id : id_list) {
-            reportLog.put(id, new ArrayList<>());
-            reportCnt.put(id, 0);
+        for (int i = 0; i < id_list.length; i++) {
+            reportLog.put(id_list[i], new ArrayList<>());
         }
 
-        // 중복이 제거된 신고 내역 처리
         for (String rep : noRedun) {
             String[] parts = rep.split(" ");
-            String reporting = parts[0]; // 신고자
-            String reported = parts[1]; // 신고당한자
+            String reporting = parts[0];
+            String reported = parts[1];
 
-            // 신고자의 신고 내역에 신고당한자를 추가
-            reportLog.get(reporting).add(reported);
+            List<String> list = reportLog.get(reporting);
+            list.add(reported);
+            reportLog.put(reporting, list);
 
-            // 신고당한자의 신고 횟수를 카운트
             reportCnt.put(reported, reportCnt.getOrDefault(reported, 0) + 1);
         }
 
-        // 신고 처리 결과를 기반으로 메일 발송 횟수 계산
         for (int i = 0; i < id_list.length; i++) {
-            String user = id_list[i];
-            List<String> reportedUsers = reportLog.get(user);
+            String id = id_list[i];
+            List<String> list = reportLog.get(id);
 
-            // 각 사용자가 신고한 대상 중에서 정지된 사용자를 찾아 알림 횟수 증가
-            for (String reported : reportedUsers) {
-                if (reportCnt.getOrDefault(reported, 0) >= k) {
+            for (String s : list) {
+
+                if (reportCnt.getOrDefault(s, 0) >= k) {
                     answer[i]++;
                 }
             }
